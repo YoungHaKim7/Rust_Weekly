e42f153722a01a23e8e02230c5a48484b0c6394b
GYoung <ytok1108@aol.com>
2023-12-26 21:10:14 +0900

    FireDBG

---
+++ 231226_debugging_FireDBG/a01_quicksort/src/main.rs
@@ -0,0 +1,35 @@
+use std::iter::repeat_with;
+
+use a01_quicksort::run;
+use firedbg_lib::fire;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    // Randdom seed
+    #[structopt(long, default_value = "2525")]
+    seed: u64,
+    // Number of random numbers to be sorted
+    #[structopt(default_value = "10")]
+    n: usize,
+}
+
+fn main() {
+    let Opt { seed, n } = Opt::from_args();
+
+    fire::dbg!(&seed);
+    fire::dbg!(&n);
+
+    fastrand::seed(seed);
+
+    let max = if n <= 10 { 100 } else { 1000 };
+
+    println!("Sort {n} numbers in ascending order");
+    let mut numbers: Vec<_> = repeat_with(|| fastrand::i32(1..max)).take(n).collect();
+
+    println!("Input : {:?}", numbers);
+    run(&mut numbers);
+    println!("Sorted : {:?}", numbers);
+
+    println!("Hello, world!");
+}
